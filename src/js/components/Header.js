0d4d31ac82351b48eea48c7847fa7885cef0e238
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-10-29 22:25:39 +0300

    renamed burger icon

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -14,7 +14,7 @@ export default class Header extends BaseComponent {
     this.container.insertAdjacentHTML('beforeend', `
         <div class="header__container">
           <a href="./" class="logo header__logo link">NewsExplorer</a>
-          <img src="../images/burger_black.svg" alt="menu icon" class="header__burger">
+          <img src="../images/burger_${this.headerColor}.svg" alt="menu icon" class="header__burger">
           <img src="../images/close_white.svg" alt="close icon" class="menu__close hidden">
           <div class="header__dark-area">
             <nav class="menu menu_${this.headerColor}">
